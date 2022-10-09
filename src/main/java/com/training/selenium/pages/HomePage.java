f3131490073e80899befd36c491c53083efa6d17
Oscar M <oscarmarquez@gmail.com>
2022-10-08 19:36:07 -0500

    update

--- src/main/java/com/training/selenium/pages/HomePage.java
+++ src/main/java/com/training/selenium/pages/HomePage.java
@@ -11,13 +11,18 @@ import java.util.HashMap;
 
 public class HomePage extends Page {
          @FindBy(css = ".menu-main__item > .is-active")
-         public WebElement homeLink;
+         public WebElement weHomeLink;
 
         @FindBy(css = ".menu-main__item:nth-child(2) > .menu-main__link")
-        public WebElement articlesLink;
+        public WebElement weArticlesLink;
 
         @FindBy(css = ".menu-main__item:nth-child(3) > .menu-main__link")
-        public WebElement recipesLink;
+        public WebElement weRecipesLink;
+
+        @FindBy(css = ".menu-account__link")
+        public WebElement weLoginLink;
+
+
          //WebDriver driver;
          Utilities util = new Utilities();
         public HomePage() throws IOException {
@@ -28,30 +33,30 @@ public class HomePage extends Page {
 
         public void goToHome() throws IOException, InterruptedException {
 
-          HashMap<String, String> homeLink = util.getORInfo("HomePage", "-homeLink");
-          WebElement weHomeLink = findByElement(homeLink.get("locator"), homeLink.get("value"));
+      //    HashMap<String, String> homeLink = util.getORInfo("HomePage", "-homeLink");
+      //    WebElement weHomeLink = findByElement(homeLink.get("locator"), homeLink.get("value"));
           weHomeLink.click();
 
         }
 
         public void goToArticles() throws InterruptedException {
-            HashMap<String, String> articlesLink = util.getORInfo("HomePage", "-articlesLink");
-            WebElement weHomeLink = findByElement(articlesLink.get("locator"), articlesLink.get("value"));
-            weHomeLink.click();
+          //  HashMap<String, String> articlesLink = util.getORInfo("HomePage", "-articlesLink");
+          //  WebElement weHomeLink = findByElement(articlesLink.get("locator"), articlesLink.get("value"));
+            weArticlesLink.click();
 
         }
 
         public void goToRecipes() throws InterruptedException {
-            HashMap<String, String> recipesLink = util.getORInfo("HomePage", "-recipesLink");
-            WebElement weHomeLink = findByElement(recipesLink.get("locator"), recipesLink.get("value"));
-            weHomeLink.click();
+        //    HashMap<String, String> recipesLink = util.getORInfo("HomePage", "-recipesLink");
+        //    WebElement weHomeLink = findByElement(recipesLink.get("locator"), recipesLink.get("value"));
+            weRecipesLink.click();
 
         }
 
         public void goToLogin() throws InterruptedException {
-            HashMap<String, String> loginLink = util.getORInfo("HomePage", "-loginLink");
-            WebElement weHomeLink = findByElement(loginLink.get("locator"), loginLink.get("value"));
-            weHomeLink.click();
+        //    HashMap<String, String> loginLink = util.getORInfo("HomePage", "-loginLink");
+        //    WebElement weHomeLink = findByElement(loginLink.get("locator"), loginLink.get("value"));
+            weLoginLink.click();
 
     }
 
--- src/test/java/com/training/selenium/testcases/HomeNavigationTest.java
+++ src/test/java/com/training/selenium/testcases/HomeNavigationTest.java
@@ -2,11 +2,18 @@ package com.training.selenium.testcases;
 
 import com.training.selenium.pages.HomePage;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Set;
 
-public class HomeNavigationTest extends HomePage  {
+public class HomeNavigationTest   {
 
     public HomeNavigationTest() throws IOException {
     }
@@ -14,10 +21,15 @@ public class HomeNavigationTest extends HomePage  {
     @Test
     public void HomeTest() throws IOException, InterruptedException {
 
-        super.goToHome();
-        super.goToArticles();
-        super.goToRecipes();
-        super.goToLogin();
+        String driversPath = System.getProperty("driversPath").replace("'", "");
+        System.setProperty("webdriver.chrome.driver", driversPath + "chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://drupal.docker.localhost:8000/");
+
+        HomePage home = PageFactory.initElements(driver, HomePage.class);
+        home.goToHome();
+        home.goToArticles();
+        home.goToRecipes();
 
     }
 
